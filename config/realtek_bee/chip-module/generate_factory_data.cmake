302617829fb401d73efeb7848299326dce436f69
patrick_li <patrick_li@realsil.com.cn>
2024-06-20 14:00:41 +0800

    factory data for zephyr

--- config/realtek_bee/chip-module/generate_factory_data.cmake
+++ config/realtek_bee/chip-module/generate_factory_data.cmake
@@ -15,160 +15,164 @@
 #
 
 
-# Create a binary file with factory data given via kConfigs.
-#
-# This function creates a list of arguments for external script and then run it to write a factory data file.
-#
-# This script can be manipulated using following kConfigs:
-# - To merge generated factory data with final zephyr.hex file set kConfig CONFIG_CHIP_FACTORY_DATA_MERGE_WITH_FIRMWARE=y
-# - To use default certification paths set CONFIG_CHIP_FACTORY_DATA_USE_DEFAULTS_CERTS_PATH=y 
-# 
-# During generation process a some file will be created in zephyr's build directory under factory subdirectory:
-# - factory_data.bin
-# - factory_data.hex
-# - DAC_cert.der
-# - DAC_cert.pem
-# - DAC_key.pem
-# - DAC_private_key.bin
-# - DAC_public_key.bin
-# - pai_cert.der
-# - onb_codes.csv
-# - pin_disc.csv
-# - qrcode.png
-# - summary.json
-#
-# [Args]:
-#   factory_data_target - a name for target to generate factory_data.
-#   script_path         - a path to script that makes a JSON factory data file from given arguments.
-#   output_path         - a path to output directory, where created JSON file will be stored.
-function(telink_create_factory_data_json factory_data_target script_path output_path)
-
-# set script args for future purpose
-set(script_args)
-## generate all script arguments
-string(APPEND script_args "--serial-num \"${CONFIG_CHIP_DEVICE_SERIAL_NUMBER}\"\n")
-string(APPEND script_args "--mfg-date \"${CONFIG_CHIP_DEVICE_MANUFACTURING_DATE}\"\n")
-string(APPEND script_args "--vendor-id ${CONFIG_CHIP_DEVICE_VENDOR_ID}\n")
-string(APPEND script_args "--product-id ${CONFIG_CHIP_DEVICE_PRODUCT_ID}\n")
-string(APPEND script_args "--vendor-name \"${CONFIG_CHIP_DEVICE_VENDOR_NAME}\"\n")
-string(APPEND script_args "--product-name \"${CONFIG_CHIP_DEVICE_PRODUCT_NAME}\"\n")
-string(APPEND script_args "--hw-ver ${CONFIG_CHIP_DEVICE_HARDWARE_VERSION}\n")
-string(APPEND script_args "--hw-ver-str \"${CONFIG_CHIP_DEVICE_HARDWARE_VERSION_STRING}\"\n")
-string(APPEND script_args "--overwrite\n")
-string(APPEND script_args "--in-tree\n")
-
-# check if Rotating Device Id Unique Id should be generated
-if(NOT CONFIG_CHIP_DEVICE_GENERATE_ROTATING_DEVICE_UID)
-    if(NOT DEFINED CONFIG_CHIP_DEVICE_ROTATING_DEVICE_UID)
-        message(FATAL_ERROR "CHIP_DEVICE_ROTATING_DEVICE_UID was not provided. To generate it use CONFIG_CHIP_DEVICE_GENERATE_ROTATING_DEVICE_UID=y")
-    else()
-        string(APPEND script_args "--rd-id-uid \"${CONFIG_CHIP_DEVICE_ROTATING_DEVICE_UID}\"\n")
+function(realtek_generate_factory_data_bin script_path output_path)
+    if(NOT EXISTS ${CHIP_ROOT}/out/spake2p)
+        message(FATAL_ERROR "Couldn't find spake2p in ${CHIP_ROOT}/out folder.
+                             Run following command from ${CHIP_ROOT} to build it:
+                             gn gen out
+                             ninja -C out spake2p")
     endif()
-else()
-    string(APPEND script_args "--enable-rotating-device-id\n")
-endif()
 
-# find chip cert tool
-find_program(chip_cert_exe NAMES chip-cert PATHS ${CHIP_ROOT}/out REQUIRED)
-string(APPEND script_args "--chip-cert-path ${chip_cert_exe}\n")
-
-# for development purpose user can use default certs instead of generating or providing them
-if(CONFIG_CHIP_FACTORY_DATA_USE_DEFAULT_CERTS)
     # convert decimal VID to its hexadecimal representation to find out certification files in repository
     math(EXPR LOCAL_VID "${CONFIG_CHIP_DEVICE_VENDOR_ID}" OUTPUT_FORMAT HEXADECIMAL)
     string(SUBSTRING ${LOCAL_VID} 2 -1 raw_vid)
     string(TOUPPER ${raw_vid} raw_vid_upper)
+
     # convert decimal PID to its hexadecimal representation to find out certification files in repository
     math(EXPR LOCAL_PID "${CONFIG_CHIP_DEVICE_PRODUCT_ID}" OUTPUT_FORMAT HEXADECIMAL)
     string(SUBSTRING ${LOCAL_PID} 2 -1 raw_pid)
     string(TOUPPER ${raw_pid} raw_pid_upper)
-    # all certs are located in ${CHIP_ROOT}/credentials/development/attestation
-    # it can be used during development without need to generate new certifications
-    string(APPEND script_args "--dac-cert \"${CHIP_ROOT}/credentials/development/attestation/Matter-Development-DAC-${raw_vid_upper}-${raw_pid_upper}-Cert.pem\"\n")
-    string(APPEND script_args "--dac-key \"${CHIP_ROOT}/credentials/development/attestation/Matter-Development-DAC-${raw_vid_upper}-${raw_pid_upper}-Key.pem\"\n")
-    string(APPEND script_args "--cert \"${CHIP_ROOT}/credentials/development/attestation/Matter-Development-PAI-${raw_vid_upper}-noPID-Cert.pem\"\n")
-    string(APPEND script_args "--key \"${CHIP_ROOT}/credentials/development/attestation/Matter-Development-PAI-${raw_vid_upper}-noPID-Key.pem\"\n")
-    string(APPEND script_args "-cd \"${CHIP_ROOT}/credentials/development/cd-certs/Chip-Test-CD-Cert.der\"\n")
-    string(APPEND script_args "--pai\n")
-else()
-    # generate PAI and DAC certs
-    string(APPEND script_args "--cert \"${CHIP_ROOT}/credentials/test/attestation/Chip-Test-PAA-NoVID-Cert.pem\"\n")
-    string(APPEND script_args "--key \"${CHIP_ROOT}/credentials/test/attestation/Chip-Test-PAA-NoVID-Key.pem\"\n")
-    string(APPEND script_args "-cd \"${CHIP_ROOT}/credentials/development/cd-certs/Chip-Test-CD-Cert.der\"\n")
-    string(APPEND script_args "--paa\n")
-endif()
 
-# find chip tool requied for generating QRCode
-find_program(chip_tool_exe NAMES chip-tool PATHS ${CHIP_ROOT}/out REQUIRED)
-string(APPEND script_args "--chip-tool-path ${chip_tool_exe}\n")
+# python3 factory_data.py -p 20202021 -d 3840 --spake2p_path=../test/spake2p 
+# --vendor-id 0xFFF1 --vendor-name "TEST_VENDOR" --product-id 0x0123 --product-name "TEST_PRODUCT" --serial-num "TEST_SN" 
+# --hw-ver 1 --hw-ver-str "1.0" --rd-id-uid 00112233445566778899aabbccddeeff --dac_cert ../test/Chip-DAC-Cert.der --dac_key 
+# ../test/Chip-DAC-Key.der --pai_cert ../test/Chip-PAI-Cert.der --cd ../test/CD.der
 
-# add Password-Authenticated Key Exchange parameters
-string(APPEND script_args "--spake2-it \"${CONFIG_CHIP_DEVICE_SPAKE2_IT}\"\n")
-string(APPEND script_args "--discriminator ${CONFIG_CHIP_DEVICE_DISCRIMINATOR}\n")
-string(APPEND script_args "--passcode ${CONFIG_CHIP_DEVICE_SPAKE2_PASSCODE}\n")
 
-# request spake2p to generate a new spake2_verifier
-find_program(spake_exe NAMES spake2p PATHS ${CHIP_ROOT}/out REQUIRED)
-string(APPEND script_args "--spake2-path ${spake_exe}\n")
+    # generate all script arguments
+    set(script_args)
+    # string(APPEND script_args "--it \"${CONFIG_CHIP_DEVICE_SPAKE2_IT}\"\n")
+    # string(APPEND script_args "--salt \"${CONFIG_CHIP_DEVICE_SPAKE2_SALT}\"\n")
+    string(APPEND script_args "--discriminator ${CONFIG_CHIP_DEVICE_DISCRIMINATOR}\n")
+    string(APPEND script_args "--passcode ${CONFIG_CHIP_DEVICE_SPAKE2_PASSCODE}\n")
+    string(APPEND script_args "--vendor-id ${CONFIG_CHIP_DEVICE_VENDOR_ID}\n")
+    string(APPEND script_args "--product-id ${CONFIG_CHIP_DEVICE_PRODUCT_ID}\n")
+    string(APPEND script_args "--vendor-name \"${CONFIG_CHIP_DEVICE_VENDOR_NAME}\"\n")
+    string(APPEND script_args "--product-name \"${CONFIG_CHIP_DEVICE_PRODUCT_NAME}\"\n")
+    string(APPEND script_args "--hw-ver ${CONFIG_CHIP_DEVICE_HARDWARE_VERSION}\n")
+    string(APPEND script_args "--hw-ver-str \"${CONFIG_CHIP_DEVICE_HARDWARE_VERSION_STRING}\"\n")
+    string(APPEND script_args "--spake2p_path \"${CHIP_ROOT}/out/spake2p\"\n")
+    string(APPEND script_args "--serial-num \"${CONFIG_CHIP_DEVICE_SERIAL_NUMBER}\"\n")
+    # string(APPEND script_args "--date \"${CONFIG_CHIP_DEVICE_MANUFACTURING_DATE}\"\n")
+    string(APPEND script_args "--rd-id-uid \"${CONFIG_CHIP_DEVICE_ROTATING_DEVICE_UID}\"\n")
+    # string(APPEND script_args "--product_finish ${CONFIG_CHIP_DEVICE_PRODUCT_FINISH}\n")
+    # string(APPEND script_args "--product_url ${CONFIG_CHIP_DEVICE_PRODUCT_URL}\n")
+    # string(APPEND script_args "--product_label ${CONFIG_CHIP_DEVICE_PRODUCT_LABEL}\n")
+    # string(APPEND script_args "--part_number ${CONFIG_CHIP_DEVICE_PART_NUMBER}\n")
+    string(APPEND script_args "--out \"${output_path}\"\n")
 
-if(CONFIG_CHIP_DEVICE_ENABLE_KEY)
-# Add optional EnableKey that triggers user-specific action.
-string(APPEND script_args "--enable-key \"${CONFIG_CHIP_DEVICE_ENABLE_KEY}\"\n")
-endif()
+    set(cd_cert ${output_path}/Chip-Test-CD-${raw_vid_upper}-${raw_pid_upper}.der)
+    set(dac_cert ${output_path}/Chip-DAC-RTK-${raw_vid_upper}-${raw_pid_upper}-Cert.der)
+    set(dac_key ${output_path}/Chip-DAC-RTK-${raw_vid_upper}-${raw_pid_upper}-Key.der)
+    set(pai_cert ${output_path}/Chip-PAI-RTK-${raw_vid_upper}-${raw_pid_upper}-Cert.der)
 
-# get code-partition factory_partition address
-dt_nodelabel(dts_partition_path NODELABEL "factory_partition")
-dt_reg_addr(factory_off PATH ${dts_partition_path})
+    string(APPEND script_args "--cd \"${cd_cert}\"\n")
+    string(APPEND script_args "--dac_cert \"${dac_cert}\"\n")
+    string(APPEND script_args "--dac_key \"${dac_key}\"\n")
+    string(APPEND script_args "--pai_cert \"${pai_cert}\"\n")
 
-# get code-partition factory_partition size
-dt_nodelabel(dts_partition_path NODELABEL "factory_partition")
-dt_reg_size(factory_size PATH ${dts_partition_path})
-
-string(APPEND script_args "--offset ${factory_off}\n")
-string(APPEND script_args "--size ${factory_size}\n")
-
-string(APPEND script_args "--output \"${output_path}\"/factory\n")
-
-set(factory_data_bin ${output_path}/factory/factory_data.bin)
-
-# execute a script to create a factory data file
-separate_arguments(separated_script_args NATIVE_COMMAND ${script_args})
-add_custom_command(
-    OUTPUT ${factory_data_bin}
-    DEPENDS ${FACTORY_DATA_SCRIPT_PATH}
-    COMMAND ${Python3_EXECUTABLE} ${FACTORY_DATA_SCRIPT_PATH} ${separated_script_args}
-    COMMENT "Generating new Factory Data..."
-    )
-add_custom_target(${factory_data_target} ALL
-    DEPENDS ${factory_data_bin}
+    # execute the script which can generate the factory_data.bin file
+    separate_arguments(separated_script_args NATIVE_COMMAND ${script_args})
+    add_custom_command(
+        OUTPUT ${output_path}/factory_data.bin
+        DEPENDS ${script_path}
+        COMMAND ${Python3_EXECUTABLE} ${script_path} ${separated_script_args}
+        COMMENT "Generating new Factory Data..."
     )
 
+    
+
+    add_custom_target(factory_data ALL
+        DEPENDS ${output_path}/factory_data.bin
+    )
+
+    if(CONFIG_CHIP_FACTORY_DATA_CERT_SOURCE_GENERATED)
+        # Make sure certificates are generated before trying to generate factory data
+        add_dependencies(factory_data chip-certificates)
+    endif()
 endfunction()
 
 
-# Generate factory data partition using given args
-#
-# 
-# During generation process a some file will be created in zephyr's build directory:
-# - merged.hex a file containing firmware and factory data merged to single file
-# - factory_data.hex a file containing only a factory data partition including proper offset
-#
-function(telink_generate_factory_data)
+function(realtek_generate_certs script_path output_path)
+    if(NOT EXISTS ${CHIP_ROOT}/out/chip-cert)
+        message(FATAL_ERROR "Couldn't find chip-cert in ${CHIP_ROOT}/out folder.
+                             Run following command from ${CHIP_ROOT} to build it:
+                             gn gen out
+                             ninja -C out chip-cert")
+    endif()
 
-find_package(Python REQUIRED)
+    # generate all script arguments
+    set(script_args)
+    string(APPEND script_args "--gen_cd\n")
+    string(APPEND script_args "--cd_type \"1\"\n")
+    string(APPEND script_args "--chip_cert_path ${CHIP_ROOT}/out/chip-cert\n")
+    string(APPEND script_args "--vendor_id ${CONFIG_CHIP_DEVICE_VENDOR_ID}\n")
+    string(APPEND script_args "--product_id ${CONFIG_CHIP_DEVICE_PRODUCT_ID}\n")
+    string(APPEND script_args "--vendor_name \"${CONFIG_CHIP_DEVICE_VENDOR_NAME}\"\n")
+    string(APPEND script_args "--product_name \"${CONFIG_CHIP_DEVICE_PRODUCT_NAME}\"\n")
+    string(APPEND script_args "--device_type \"${CONFIG_CHIP_DEVICE_TYPE}\"\n")
+    string(APPEND script_args "--output \"${output_path}\"\n")
 
-# CHIP_ROOT must be provided as a reference set all localization of scripts
-if(NOT CHIP_ROOT)
-message(FATAL_ERROR "CHIP_ROOT variable is not set, please add it to CMakeLists.txt file")
-endif()
 
-# Localize all scripts needed to generate factory data partition
-set(FACTORY_DATA_SCRIPT_PATH ${CHIP_ROOT}/scripts/tools/telink/mfg_tool.py)
-set(OUTPUT_FILE_PATH ${APPLICATION_BINARY_DIR}/zephyr)
 
-# create a binary file with all factory data
-telink_create_factory_data_json(factory_data
-                                    ${FACTORY_DATA_SCRIPT_PATH}
-                                    ${OUTPUT_FILE_PATH})
+    # convert decimal VID to its hexadecimal representation to find out certification files in repository
+    math(EXPR LOCAL_VID "${CONFIG_CHIP_DEVICE_VENDOR_ID}" OUTPUT_FORMAT HEXADECIMAL)
+    string(SUBSTRING ${LOCAL_VID} 2 -1 raw_vid)
+    string(TOUPPER ${raw_vid} raw_vid_upper)
 
+    # convert decimal PID to its hexadecimal representation to find out certification files in repository
+    math(EXPR LOCAL_PID "${CONFIG_CHIP_DEVICE_PRODUCT_ID}" OUTPUT_FORMAT HEXADECIMAL)
+    string(SUBSTRING ${LOCAL_PID} 2 -1 raw_pid)
+    string(TOUPPER ${raw_pid} raw_pid_upper)
+
+
+    # execute the script which can generate the certificates
+    separate_arguments(separated_script_args NATIVE_COMMAND ${script_args})
+
+    message("separated_script_args: ${separated_script_args} \r\n raw_vid_upper ${raw_vid_upper} raw_pid_upper ${raw_pid_upper}")
+    add_custom_command(
+        OUTPUT
+        ${output_path}/Chip-PAI-NXP-${raw_vid_upper}-${raw_pid_upper}-Cert.pem
+        ${output_path}/Chip-PAI-NXP-${raw_vid_upper}-${raw_pid_upper}-Key.pem
+        ${output_path}/Chip-DAC-NXP-${raw_vid_upper}-${raw_pid_upper}-Cert.pem
+        ${output_path}/Chip-DAC-NXP-${raw_vid_upper}-${raw_pid_upper}-Key.pem
+        ${output_path}/Chip-PAI-NXP-${raw_vid_upper}-${raw_pid_upper}-Cert.der
+        ${output_path}/Chip-PAI-NXP-${raw_vid_upper}-${raw_pid_upper}-Key.der
+        ${output_path}/Chip-DAC-NXP-${raw_vid_upper}-${raw_pid_upper}-Cert.der
+        ${output_path}/Chip-DAC-NXP-${raw_vid_upper}-${raw_pid_upper}-Key.der
+        DEPENDS ${script_path}
+        COMMAND ${Python3_EXECUTABLE} ${script_path} ${separated_script_args}
+        COMMENT "Generating new certificates..."
+    )
+    add_custom_target(chip-certificates ALL
+        DEPENDS
+        ${output_path}/Chip-PAI-NXP-${raw_vid_upper}-${raw_pid_upper}-Cert.pem
+        ${output_path}/Chip-PAI-NXP-${raw_vid_upper}-${raw_pid_upper}-Key.pem
+        ${output_path}/Chip-DAC-NXP-${raw_vid_upper}-${raw_pid_upper}-Cert.pem
+        ${output_path}/Chip-DAC-NXP-${raw_vid_upper}-${raw_pid_upper}-Key.pem
+        ${output_path}/Chip-PAI-NXP-${raw_vid_upper}-${raw_pid_upper}-Cert.der
+        ${output_path}/Chip-PAI-NXP-${raw_vid_upper}-${raw_pid_upper}-Key.der
+        ${output_path}/Chip-DAC-NXP-${raw_vid_upper}-${raw_pid_upper}-Cert.der
+        ${output_path}/Chip-DAC-NXP-${raw_vid_upper}-${raw_pid_upper}-Key.der
+    )
+endfunction()
+
+function(realtek_generate_factory_data)
+    find_package(Python REQUIRED)
+
+    # CHIP_ROOT must be provided as a reference set all localization of scripts
+    if(NOT CHIP_ROOT)
+        message(FATAL_ERROR "CHIP_ROOT variable is not set, please add it to CMakeLists.txt file")
+    endif()
+
+    # Localize all scripts needed to generate factory data partition
+    set(GENERATE_CERTS_SCRIPT_PATH ${CHIP_ROOT}/scripts/tools/realtek/generate_certs.py)
+    message("APPLICATION_BINARY_DIR: ${APPLICATION_BINARY_DIR}")
+    set(OUTPUT_FILE_PATH ${APPLICATION_BINARY_DIR}/zephyr)
+    set(GENERATE_FDATA_SCRIPT_PATH ${CHIP_ROOT}/scripts/tools/realtek/factory_data.py)
+
+    # Generate certificates
+    realtek_generate_certs(${GENERATE_CERTS_SCRIPT_PATH} ${OUTPUT_FILE_PATH})
+
+    realtek_generate_factory_data_bin(${GENERATE_FDATA_SCRIPT_PATH} ${OUTPUT_FILE_PATH})
 endfunction()
